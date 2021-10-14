b61a9054e3df56b5d5820fef9a992484500c40a8
arthurfurtunato <arthurfurtunato@gmail.com>
2021-10-14 08:26:46 -0700

    Views e urls feitas

--- generator/urls.py
+++ generator/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('password/', views.password, name='password'),
+    path('generator/', views.generator, name='generator'),
+    path('generator/password/', views.password, name='password'),
 ]
--- generator/views.py
+++ generator/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
+import string, random
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {
-        'range': range(1, 11),
+    return render(request, 'index.html')
+
+def generator(request):
+    return render(request, 'generator.html', {
+        'range': range(6, 15),
     })
 
 def password(request):
-    return render(request, 'password.html')
+    length = request.POST.get('length')
+    password1 = ''
+    password2 = ''
+    password3 = ''
+    if 'uppercase' in request.POST:
+        password1 = string.ascii_uppercase
+
+    if 'special' in request.POST:
+        password2 = string.punctuation
+
+    if 'numbers' in request.POST:
+        password3 = string.digits
+    
+    password = ''.join(random.choice(string.ascii_lowercase + password1 + password2 + password3) for i in range(int(length)))
+
+    return render(request, 'password.html',{
+        'password': password,
+    })
